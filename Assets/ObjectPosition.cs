e71b7e67e531f6a821f2825203ae7c95bc17fbbe
nik-marinenko <nik.marinenko@gmail.com>
2023-03-11 17:18:22 +0300

    time travel logic comleted

--- Assets/ObjectPosition.cs
+++ Assets/ObjectPosition.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class ObjectPosition : MonoBehaviour
 {
-    public float currentPositionX;
-    public float currentPositionY;
-    private int currentPositionIndex = 0;
-
+    public float futureCurrentPositionX;
+    public float futureCurrentPositionY;
+    private float pastCurrentPositionX;
+    private float pastCurrentPositionY;
+    private int pastCurrentPositionIndex = 0;
+    private int futureCurrentPositionIndex = 0;
+    private Vector3 holdPosition;
 
     public void Update()
     {
@@ -16,25 +19,40 @@ public class ObjectPosition : MonoBehaviour
 
         if (Swap.isFuture == true)
         {
+            if (futureCurrentPositionIndex == 0)
+            {
+                this.transform.position = new Vector3(futureCurrentPositionX, futureCurrentPositionY, 0);
+                futureCurrentPositionIndex++;
+            }
+            futureCurrentPositionX = this.transform.position.x;
+            futureCurrentPositionY = this.transform.position.y;
             if (transform.parent != null)
             {
-                currentPositionX = GameObject.Find("HoldSpot").transform.position.x;
-                currentPositionY = GameObject.Find("HoldSpot").transform.position.y;
+                pastCurrentPositionX = GameObject.Find("HoldSpot").transform.position.x;
+                pastCurrentPositionY = GameObject.Find("HoldSpot").transform.position.y;
             }
-            currentPositionIndex = 0;
+            pastCurrentPositionIndex = 0;
         }
         else if(Swap.isFuture==false)
         {
-            
-            if (currentPositionIndex==0) 
+            futureCurrentPositionIndex = 0;
+            if (pastCurrentPositionIndex==0) 
             {
-                this.transform.position = new Vector3(currentPositionX, currentPositionY, 0);
-                currentPositionIndex++;
+                this.transform.position = new Vector3(pastCurrentPositionX, pastCurrentPositionY, 0);
+                holdPosition = this.transform.position;
+                pastCurrentPositionIndex++;
             }
             
-            currentPositionX = this.transform.position.x;
-            currentPositionY = this.transform.position.y;
-          
+            pastCurrentPositionX = this.transform.position.x;
+            pastCurrentPositionY = this.transform.position.y;
+            //если положение кубика изменилось
+            if(this.transform.position != holdPosition)
+            {
+                futureCurrentPositionX = pastCurrentPositionX;
+                futureCurrentPositionY = pastCurrentPositionY;
+            }
+            
+
 
         } 
     }
